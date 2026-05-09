7cef39448c534a186dfb4caa104f0bc105408832
Ponthius <ponthiuspaul@gmail.com>
2026-05-09 13:40:39 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oswald_POS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
